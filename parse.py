b010cf9201c0ff8ba908c2871e85f266d3cb2099
Tamara Slosarek <tamara.slosarek@hpi.de>
2022-06-07 15:28:51 +0200

    Write JSON

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 import pdfplumber
+import json
 import os
 
 DATA_PATH = "data"
@@ -7,7 +8,7 @@ PDF_ENDING = ".pdf"
 JSON_ENDING = ".json"
 SPECIAL_WILDTYPE_ALLELE_NAMES = {}
 
-def getGenotypeDetailTable(pdfPath):
+def getGenotypeTable(pdfPath):
     genotypeTable = None
     with pdfplumber.open(pdfPath) as pdf:
         for page in pdf.pages:
@@ -23,36 +24,46 @@ def getGenotypeDetailTable(pdfPath):
 
 def getDiplotype(gene, diplotypeText):
     diplotype = None
-    if 'wildtype' in diplotypeText:
-        wildtypeAlleleName = '*1'
-        if gene in SPECIAL_WILDTYPE_ALLELE_NAMES:
-            wildtypeAlleleName = SPECIAL_WILDTYPE_ALLELE_NAMES[gene]
-        diplotypeText = diplotypeText.replace('wildtype', wildtypeAlleleName)
+    # if 'wildtype' in diplotypeText:
+    #     wildtypeAlleleName = '*1'
+    #     if gene in SPECIAL_WILDTYPE_ALLELE_NAMES:
+    #         wildtypeAlleleName = SPECIAL_WILDTYPE_ALLELE_NAMES[gene]
+    #     diplotypeText = diplotypeText.replace('wildtype', wildtypeAlleleName)
     if ' or ' in diplotypeText:
         diplotype = diplotypeText.split(' or ')
-        unambiguous = False
     else:
         diplotype = [diplotypeText]
-        unambiguous = True        
-    return diplotype, unambiguous
+    return diplotype
 
-def getStarAllelesFromGenotypeTable(genotypeTable):
+def getPhenotype(gene, phenotypeText):
+    phenotypeText = phenotypeText.replace('\n', ' ').replace('  ', ' ')
+    if 'METABOLIZER' in phenotypeText:
+        phenotypeText = phenotypeText.replace(gene, '').strip()
+    return phenotypeText
+
+def getStarAlleleData(genotypeTable):
     starAlleles = {}
     for row in genotypeTable[1:]:
         gene = row[0]
         diplotypeText = row[1]
-        diplotype, unambiguous = getDiplotype(gene, diplotypeText)
+        diplotype = getDiplotype(gene, diplotypeText)
+        phenotypeText = row[2]
+        phenotype = getPhenotype(gene, phenotypeText)
         starAlleles[gene] = {
             'diplotype': diplotype,
-            'unambiguous': unambiguous
+            'phenotype': phenotype
         }
     return starAlleles
 
 for fileName in os.listdir(DATA_PATH):
     if fileName.endswith(PDF_ENDING):
         filePath = os.path.join(DATA_PATH, fileName)
-        genotypeTable = getGenotypeDetailTable(filePath)
-        starAlleles = getStarAllelesFromGenotypeTable(genotypeTable)
-        # TODO: Check wildtype alleles (see constants above)
-        # TODO: Write to JSON
-        # TODO: Double check diplotypes (maybe with CPIC API, e.g., https://api.cpicpgx.org/v1/diplotype?genesymbol=eq.CYP2B6&diplotype=eq.*1/*6)
+        genotypeTable = getGenotypeTable(filePath)
+        starAlleleData = getStarAlleleData(genotypeTable)
+        jsonPath = filePath.replace(
+            DATA_PATH, RESULTS_PATH, 1).replace(PDF_ENDING, JSON_ENDING)
+        with open(jsonPath, 'w') as jsonFile:
+            json.dump(starAlleleData, jsonFile)
+        # TODO: Check wildtype alleles (see above)
+        # TODO: Check CPIC definition of ambiguous diplotypes
+        # TODO: Prettier phenotype and check with CPIC API, e.g., https://api.cpicpgx.org/v1/diplotype?genesymbol=eq.CYP2B6&diplotype=eq.*1/*6
